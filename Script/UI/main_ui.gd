477f57ec7b1c92b88367b937a5c3df000b5ecf46
Zulta <69368148+ZultaCulu@users.noreply.github.com>
2025-10-25 19:19:23 -0600

    setup main UI buttons

--- Script/UI/main_ui.gd
+++ Script/UI/main_ui.gd
@@ -1,1 +1,26 @@
 extends Control
+
+#Button Nodes
+@onready var fightbtn := $Fight
+@onready var movebtn := $Move
+@onready var itembtn := $Item
+@onready var defendbtn := $Defend
+@onready var fleebtn := $Flee
+
+func _ready() -> void:
+	fightbtn.grab_focus()
+
+func _on_fight_pressed() -> void:
+	LogFox.info("Fight Button Pressed", fightbtn)
+
+func _on_move_pressed() -> void:
+	LogFox.info("Move Button Pressed", movebtn)
+
+func _on_item_pressed() -> void:
+	LogFox.info("Item Button Pressed", itembtn)
+
+func _on_defend_pressed() -> void:
+	LogFox.info("Defend Button Pressed", defendbtn)
+
+func _on_flee_pressed() -> void:
+	LogFox.info("Flee Button Pressed", fleebtn)
